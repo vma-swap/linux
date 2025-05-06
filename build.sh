87242e204a42c06f723b2671cce05890af6df23d
Your Name <you@example.com>
2025-05-06 17:57:08 +0300

    improve build script

--- build.sh
+++ build.sh
@@ -1,9 +1,14 @@
 #!/bin/bash
-make -j$(nproc)
+make -j$(nproc) M=mm
 #check return code and exit prematurely if it fails
 if [ $? -ne 0 ]; then
     echo "Build failed"
     exit 1
 fi
-sudo make modules_install
+make -j$(nproc) bzImage
+#check return code and exit prematurely if it fails
+if [ $? -ne 0 ]; then
+    echo "Build failed"
+    exit 1
+fi
 sudo make install
