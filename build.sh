cb84337d6e7a802d3d4cc09fee2d103bd010f64d
Your Name <you@example.com>
2025-05-09 13:07:27 +0300

    improve build scripts

--- build.sh
+++ build.sh
@@ -1,4 +1,29 @@
 #!/bin/bash
+# if install was passed
+if [ "$1" == "install" ]; then
+    echo -e "\033[0;32mInstalling release kernel...\033[0m"
+    sudo make M=mm modules_install
+    sudo make install
+    exit 0
+fi
+if [ "$1" == "release" ]; then
+# echo in color green
+    echo -e "\033[0;32mBuilding release kernel...\033[0m"
+    make -j$(nproc)
+    # if [ $? -ne 0 ]; then
+    #     echo "Build failed"
+    #     exit 1
+    # fi
+    # make -j$(nproc) bzImage
+    # if [ $? -ne 0 ]; then
+    #     echo "Build failed"
+    #     exit 1
+    # fi
+    sudo make -j$(nproc) modules_install
+    sudo make install
+    exit 0
+fi
+echo -e "\033[0;32mBuilding dev kernel...\033[0m"
 make -j$(nproc) M=mm
 #check return code and exit prematurely if it fails
 if [ $? -ne 0 ]; then
@@ -11,4 +36,3 @@ if [ $? -ne 0 ]; then
     echo "Build failed"
     exit 1
 fi
-sudo make install
