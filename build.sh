1bfe3ed0330695aa034dc009a0fa5b164f57810f
Daniel Bransky <d.s.bransky@gmail.com>
2026-01-17 14:14:28 +0200

    my scripts. not for upsteam

---
+++ build.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+# if install was passed
+if [ "$1" == "install" ]; then
+    echo -e "\033[0;32mInstalling release kernel...\033[0m"
+    sudo make modules_install
+    sudo make install
+    exit 0
+fi
+if [ "$1" == "release" ]; then
+# echo in color green
+    echo -e "\033[0;32mBuilding release kernel...\033[0m"
+    make ARCH=x86_64 -j$(nproc)
+    if [ $? -ne 0 ]; then
+        echo "Build failed"
+        exit 1
+    fi
+    sudo make ARCH=x86_64 -j$(nproc) modules_install
+    sudo make install
+    exit 0
+fi
+echo -e "\033[0;32mBuilding dev kernel...\033[0m"
+make ARCH=x86_64 -j$(nproc) bzImage
+#check return code and exit prematurely if it fails
+if [ $? -ne 0 ]; then
+    echo "Build failed"
+    exit 1
+fi
